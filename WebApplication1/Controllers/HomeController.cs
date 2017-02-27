69650bffcc61eb57d72b8f47106a91631a5c9871
dayran <dalvarez@pikmykid.com>
2017-02-27 12:43:00 -0500

    sdfsdf

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page. <br/> It's work fine";
+            ViewBag.Message = "Your application description page";
 
             return View();
         }
